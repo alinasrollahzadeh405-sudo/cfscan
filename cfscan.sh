3f1ddf1bdc507eccce22c314c22cc9be149f32ee
ali nasrollahzadeh <alinasrollahzadeh405@gmail.com>
2026-08-16 21:34:14 +0330

    Add files via upload

---
+++ cfscan.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "welcome to cfscan"
+
+curl -s -o ranges.txt "https://raw.githubusercontent.com/alinasrollahzadeh405-sudo/cfscan/main/ranges.txt"
+
+rm -f ipcf.txt
+
+read -p "Please enter port for testing ips (default 443): " port
+port=${port:-443}
+
+while read -r ranges; do
+    echo "finding ips..."
+    ipcf=$(prips "$ranges" | shuf)
+    
+    for ip in $ipcf; do
+        echo "scanning ips..."
+
+        if nmap "$ip" -p "$port" &> /dev/null; then
+            echo "ip is alive! testing icmp..."
+            if ping -c4 -W 4 "$ip" &> /dev/null; then
+                echo "icmp is ok! ip saved!"
+                echo "$ip" >> ipcf.txt
+            fi
+        fi
+    done
+done < ranges.txt
